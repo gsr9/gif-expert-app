ddb3a6445adcd11af61800166d3395b69973a2ed
Serra-Ruidiaz-Guillermo <gsr9@alu.ua.es>
2021-07-04 12:49:33 +0200

    First approach list

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 function GifExpertApp() {
   //   const categories = ["One punch", "Samurai X", "Dragon Ball"];
 
   const [categories, setCategories] = useState(["One punch", "Samurai X", "Dragon Ball"]);
 
-  const handleAdd = () => {
-    const newCategory = "Detective Conan";
-    setCategories(cats => [...cats, newCategory]);
-  };
 
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
 
-      <button onClick={handleAdd}>Agregar</button>
+      <AddCategory setCategories={setCategories} />
 
       <ul>
         {categories.map((c) => (
